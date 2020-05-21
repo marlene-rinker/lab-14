a42361126e2e6c870eb1011eaf071e6ba6bf6687
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-05-21 12:13:03 -0700

    added join

--- data/migrations/1590087211848-separating-bookshelves-from-books.sql
+++ data/migrations/1590087211848-separating-bookshelves-from-books.sql
@@ -9,3 +9,5 @@ UPDATE books SET bookshelf_id=shelf.id FROM (SELECT * FROM bookshelves) AS shelf
 ALTER TABLE books DROP COLUMN bookshelf;
 
 ALTER TABLE books ADD CONSTRAINT fk_bookshelves FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id);
+
+SELECT books.id, title, author, name FROM books INNER JOIN bookshelves ON books.bookshelf_id=bookshelves.id;
